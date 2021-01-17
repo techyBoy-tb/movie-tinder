c61a47dd5d7afa4a79f06c140d9aa09b73172165
techyBoy-tb <garethclifford9519@gmail.com>
2021-01-17 13:04:13 +0000

    GC - Refactor to comply with Flutter best practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_tinder/bloc/auth/auth_bloc.dart';
 import 'package:movie_tinder/bloc/blocDelegate.dart';
+import 'package:movie_tinder/repositories/userRepository.dart';
 import 'package:movie_tinder/ui/screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  final UserRepository _userRepository = UserRepository();
+
   BlocSupervisor.delegate = SimpleBlocDelegate();
 
-  runApp(Home());
+  runApp(
+    BlocProvider(
+      create: (context) => AuthBloc(userRepository: _userRepository)
+        ..add(AppStarted()),
+      child: Home(userRepository: _userRepository)
+    )
+  );
 }
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -9,49 +9,80 @@ import 'package:movie_tinder/ui/screens/signUp.dart';
 import 'package:movie_tinder/ui/screens/splash.dart';
 import 'package:movie_tinder/ui/widgets/tabs.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+// class Home extends StatefulWidget {
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   final UserRepository _userRepository = UserRepository();
+//   AuthBloc _authBloc;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Firebase.initializeApp().whenComplete(() => {
+//       _authBloc = AuthBloc(userRepository: _userRepository);
+//       _authBloc.add(AppStarted());
+//     // });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (context) => _authBloc,
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: BlocBuilder(
+//           bloc: _authBloc,
+//           builder: (BuildContext context, AuthState state) {
+//             if (state is Uninitialised) {
+//               return SplashScreen();
+//             } else if (state is Authenticated) {
+//               return Tabs();
+//             } else if (state is AuthenticatedButNotSet) {
+//               return Profile(
+//                 userRepository: _userRepository,
+//                 userId: state.userId
+//               );
+//             } else if(state is Unauthenticated) {
+//               return Login(userRepository: _userRepository);
+//             } else {
+//               return Container();
+//             }
+//           }
+//         )
+//       ),
+//     );
+//   }
+// }
 
-class _HomeState extends State<Home> {
-  final UserRepository _userRepository = UserRepository();
-  AuthBloc _authBloc;
+class Home extends StatelessWidget {
+  final UserRepository _userRepository;
 
-  @override
-  void initState() {
-    super.initState();
-    // Firebase.initializeApp().whenComplete(() => {
-      _authBloc = AuthBloc(userRepository: _userRepository);
-      _authBloc.add(AppStarted());
-    // });
-  }
+  Home({@required UserRepository userRepository}) : assert (userRepository != null), _userRepository = userRepository;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => _authBloc,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: BlocBuilder(
-          bloc: _authBloc,
-          builder: (BuildContext context, AuthState state) {
-            if (state is Uninitialised) {
-              return SplashScreen();
-            } else if (state is Authenticated) {
-              return Tabs();
-            } else if (state is AuthenticatedButNotSet) {
-              return Profile(
-                userRepository: _userRepository,
-                userId: state.userId
-              );
-            } else if(state is Unauthenticated) {
-              return Login(userRepository: _userRepository);
-            } else {
-              return Container();
-            }
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is Uninitialised) {
+            return SplashScreen();
+          } else if (state is Authenticated) {
+            return Tabs();
+          } else if (state is AuthenticatedButNotSet) {
+            return Profile(
+              userRepository: _userRepository,
+              userId: state.userId
+            );
+          } else if(state is Unauthenticated) {
+            return Login(userRepository: _userRepository);
+          } else {
+            return Container();
           }
-        )
+        },
       ),
     );
   }
